a606b717a928cea2f79fead37412b31a76ea378a
emilyreba <68654895+emilyreba@users.noreply.github.com>
2020-07-31 09:40:53 -0400

    hopefully final version!

--- ReadFeed/ExodusViewController.swift
+++ ReadFeed/ExodusViewController.swift
@@ -10,6 +10,15 @@ import UIKit
 
 class ExodusViewController: UIViewController {
     
+    @IBOutlet weak var labelOneOne: UILabel!
+    
+    @IBOutlet weak var labelTwoTwo: UILabel!
+    
+    @IBOutlet weak var labelThreeThree: UILabel!
+    
+    @IBOutlet weak var labelFourFour: UILabel!
+    
+    @IBOutlet weak var labelFiveFive: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,23 +28,44 @@ class ExodusViewController: UIViewController {
     }
     
     @IBAction func bookOneOne(_ sender: UIButton) {
-       
+        labelOneOne.text = "🐛"
+        labelTwoTwo.text = " "
+        labelThreeThree.text = " "
+        labelFourFour.text = " "
+        labelFiveFive.text = " "
     }
     
     @IBAction func bookTwoTwo(_ sender: UIButton) {
-       
+       labelOneOne.text = "🐛"
+       labelTwoTwo.text = "🐛"
+       labelThreeThree.text = " "
+       labelFourFour.text = " "
+       labelFiveFive.text = " "
     }
     
     @IBAction func bookThreeThree(_ sender: UIButton) {
-        
+        labelOneOne.text = "🐛"
+        labelTwoTwo.text = "🐛"
+        labelThreeThree.text = "🐛"
+        labelFourFour.text = " "
+        labelFiveFive.text = " "
     }
     
     @IBAction func bookFourFour(_ sender: UIButton) {
-
+        labelOneOne.text = "🐛"
+        labelTwoTwo.text = "🐛"
+        labelThreeThree.text = "🐛"
+        labelFourFour.text = "🐛"
+        labelFiveFive.text = " "
     }
     
     
     @IBAction func bookFiveFive(_ sender: UIButton) {
+        labelOneOne.text = "🐛"
+        labelTwoTwo.text = "🐛"
+        labelThreeThree.text = "🐛"
+        labelFourFour.text = "🐛"
+        labelFiveFive.text = "🐛"
     }
     
 }
